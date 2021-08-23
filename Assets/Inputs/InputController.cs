dcfb16fe1cbb8c7d9792fcff90a0885d1a5d1d47
Feeziks <feeziksg@gmail.com>
2021-08-23 10:29:30 -0600

    Basic Movement Idea is sort of there

---
+++ Assets/Inputs/InputController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InputController : MonoBehaviour
+{
+  public Vector2 movement;
+  public bool click;
+
+
+  public void OnMovement(InputAction.CallbackContext context)
+  {
+    movement = context.ReadValue<Vector2>();
+  }
+
+  public void OnClick(InputAction.CallbackContext context)
+  {
+    if(context.started) //Only works when click is initiated, will need to determine if we need a seperate "click held" event
+    {
+      click = true;
+    }
+  }
+}
---
+++ Assets/Scripts/CameraControls.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControls : MonoBehaviour
+{
+  public InputController inputs;
+  public float rotationSpeed;
+
+
+
+
+
+  private void Update()
+  {
+    HandleMovement();
+  }
+
+
+  private void HandleMovement()
+  {
+    if(inputs.movement != Vector2.zero)
+    {
+      //Rotate the camera parent object and the camera will follow
+      transform.Rotate(new Vector3(0f, inputs.movement.x, inputs.movement.y), rotationSpeed * Time.deltaTime);
+    }
+  }
+
+
+
+
+}
