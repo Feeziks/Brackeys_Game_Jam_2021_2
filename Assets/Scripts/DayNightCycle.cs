3f7edec41003ce10271801c739396fb753ca227c
Feeziks <feeziksg@gmail.com>
2021-08-23 10:45:00 -0600

    Very basic day/night cycle

---
+++ Assets/Scripts/DayNightCycle.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DayNightCycle : MonoBehaviour
+{
+  public Light sun;
+  public Light moon;
+  public Vector3 center;
+
+  public float rotationSpeed;
+
+  private void Start()
+  {
+    
+  }
+
+
+  private void Update()
+  {
+    //Rotate the lights
+
+    //TODO: Pause / time speed changes
+
+    sun.transform.RotateAround(center, Vector3.up, rotationSpeed * Time.deltaTime);
+    moon.transform.RotateAround(center, Vector3.up, rotationSpeed * Time.deltaTime);
+
+  }
+}
